1305ce896048429afc98f02b7867722155984239
amaan <amaan.ak471@gmail.com>
2025-04-15 11:48:22 +0530

    commit before changing display manager

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -1,7 +1,210 @@
+import { useState } from "react";
+
+import image from "../assets/image.png";
+import Dropdown from "react-bootstrap/Dropdown";
+
 const ProductDetail = () => {
+  const [filterChange, handleFilterChange] = useState("Select");
+
   return (
     <div className="container py-md-5 py-2">
-      <div className="text-center">hello</div>
+      <div className="d-flex gap-5">
+        <div className="product-image">
+          <img src={image} alt="image" />
+        </div>
+        <div className="product-info">
+          <h3>Product Title</h3>
+          <h5 className="text-secondary">By Brand</h5>
+          <h4 className="star-rating">&#11088;&#11088;&#11088;&#11088;</h4>
+          <h5 className="fw-bold">50.99$</h5>
+          <h5 className="discount">20% Off</h5>
+          <h5 className="stock-indicator mb-5">[In Stock]</h5>
+          <h5 className="size">Size : [S][M][L][XL]</h5>
+          <h5 className="color">
+            <span className="color-indicator" style={{ color: "red" }}></span>
+            <span className="color-indicator" style={{ color: "blue" }}></span>
+            <span className="color-indicator" style={{ color: "green" }}></span>
+            <span className="color-indicator" style={{ color: "Black" }}></span>
+            <span
+              className="color-indicator"
+              style={{ color: "white", border: "thin solid black" }}
+            ></span>
+          </h5>
+          <h5 className="my-4 mb-5">Quantity : [-] 1 [+]</h5>
+          <button className="btn btn-primary d-block my-2">Add to cart</button>
+          <button className="btn btn-secondary d-block my-2 mb-4">
+            Buy Now
+          </button>
+          <h6>&#9989;Free shipping</h6>
+          <h6> &#9989;Easy Returns</h6>
+        </div>
+      </div>
+
+      <div className="prod-description">
+        <div className="my-5 mb-4">
+          <h4 className="pb-3">Description</h4>
+          <p>
+            This casual cotton t-shirt is perfect for daily wear. Featuring a
+            soft and breathable fabric, it offers all-day comfort with a relaxed
+            fit. Pair it with jeans or joggers for an effortless look.{" "}
+          </p>
+        </div>
+        <hr className="hr" />
+        <div className="my-4">
+          <h4 className="pb-3">Fabric & Care Instructions</h4>
+          <p>- Fabric: 100% Cotton </p>
+          <p>- Soft-touch material</p>
+          <div>
+            <p>- Wash Care:</p>
+            <ul>
+              <li>Machine wash cold</li>
+              <li>Do not bleach</li>
+              <li>Iron on low heat</li>
+              <li>Do not dry clean</li>
+            </ul>
+          </div>
+        </div>
+        <hr className="hr" />
+        <div className="my-4">
+          <h4 className="pb-3">Size & Fit</h4>
+          <p>- Fit: Regular </p>
+          <p>- Model is 6&apos;1&quot; and wearing size L</p>
+          <p>
+            - Refer to the size chart for exact measurements{" "}
+            <i>[View Size Chart]</i> →
+          </p>
+        </div>
+        <hr className="hr" />
+        <div className="my-4">
+          <h4>Specifications</h4>
+          <div
+            className="d-grid pt-3"
+            style={{ gridTemplateColumns: "1fr 1fr", maxWidth: "350px" }}
+          >
+            <p>Sleeve Length</p>
+            <p>Short Sleeves</p>
+            <p>Neck Type</p>
+            <p>Crew Neck</p>
+            <p>Pattern</p>
+            <p>Solid</p>
+            <p>Occasion</p>
+            <p>Casual</p>
+            <p>Country of Origin</p>
+            <p>India</p>
+          </div>
+        </div>
+      </div>
+
+      <hr className="bold" />
+
+      <div className="reviews my-4">
+        <h1 className="pb-3">Customer reviews</h1>
+        <div className="rating-overall">
+          <div className="fs-4 mb-2">★★★★☆ 4.2 out of 5</div>
+          <p>Based on 123 reviews</p>
+          <div
+            className="rating-category rounded border  p-3 w-100"
+            style={{ maxWidth: "410px" }}
+          >
+            <div className="d-flex align-items-center justify-content-between gap-2">
+              <div
+                className="position-relative w-100"
+                style={{ maxWidth: "300px", height: "12px" }}
+              >
+                <div className="w-100 rounded bg-white border border-3 h-100"></div>
+                <div
+                  className="bg-warning rounded position-absolute top-0 left-0 h-100"
+                  style={{ width: "75%" }}
+                ></div>
+              </div>
+              <p className="m-0 p-0">5★ (80)</p>
+            </div>
+            <div className="d-flex align-items-center justify-content-between gap-2">
+              <div
+                className="position-relative w-100"
+                style={{ maxWidth: "300px", height: "12px" }}
+              >
+                <div className="w-100 rounded bg-white border border-3 h-100"></div>
+                <div
+                  className="bg-warning rounded position-absolute top-0 left-0 h-100"
+                  style={{ width: "60%" }}
+                ></div>
+              </div>
+              <p className="m-0 p-0">4★ (67)</p>
+            </div>
+            <div className="d-flex align-items-center justify-content-between gap-2">
+              <div
+                className="position-relative w-100"
+                style={{ maxWidth: "300px", height: "12px" }}
+              >
+                <div className="w-100 rounded bg-white border border-3 h-100"></div>
+                <div
+                  className="bg-warning rounded position-absolute top-0 left-0 h-100"
+                  style={{ width: "90%" }}
+                ></div>
+              </div>
+              <p className="m-0 p-0">3★ (76)</p>
+            </div>
+            <div className="d-flex align-items-center justify-content-between gap-2">
+              <div
+                className="position-relative w-100"
+                style={{ maxWidth: "300px", height: "12px" }}
+              >
+                <div className="w-100 rounded bg-white border border-3 h-100"></div>
+                <div
+                  className="bg-warning rounded position-absolute top-0 left-0 h-100"
+                  style={{ width: "10%" }}
+                ></div>
+              </div>
+              <p className="m-0 p-0">2★ (10)</p>
+            </div>
+            <div className="d-flex align-items-center justify-content-between gap-2">
+              <div
+                className="position-relative w-100"
+                style={{ maxWidth: "300px", height: "12px" }}
+              >
+                <div className="w-100 rounded bg-white border border-3 h-100"></div>
+                <div
+                  className="bg-warning rounded position-absolute top-0 left-0 h-100"
+                  style={{ width: "5%" }}
+                ></div>
+              </div>
+              <p className="m-0 p-0">1★ (5)</p>
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-primary my-3">Write a review</button>
+
+        <div className="my-3 fs-5 d-flex gap-2 align-items-center">
+          <b className="p-0 m-0">Filter By</b>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              {filterChange}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item
+                href="#"
+                onClick={() => handleFilterChange("Most Recent")}
+              >
+                Most Recent
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#"
+                onClick={() => handleFilterChange("Highest rating")}
+              >
+                Highest rating
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="#"
+                onClick={() => handleFilterChange("Lowest rating")}
+              >
+                Lowest rating
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </div>
     </div>
   );
 };
